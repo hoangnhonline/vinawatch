1afb2fdb502c34404477c37ea8b0700c4e617a2a
HOANGNH <hoangnhonline@gmail.com>
2016-01-10 23:02:04 +0700

    abc

--- blocks/menutop.php
+++ blocks/menutop.php
@@ -31,11 +31,63 @@ $cateTypeArr = $model->getListCateType();
 <?php }}?>                
     </ul>
     <div class="banner-main">
-      <img src="http://vinawatch.vn/upload/images/2015/12/12/banner2-2.jpg">
-    </div>
-    <div class="banner-main2">
-      <img src="http://vinawatch.vn/upload/images/2015/12/12/bannerleft-4.jpg">
-      <img src="http://vinawatch.vn/upload/images/2015/12/12/bannerleft-5.jpg">
-    </div>
+              
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+           
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <?php $arr = $model->getListBannerByPosition(1,-1); ?>
+            <?php
+              if(!empty($arr)){           
+                $countSide = 0;
+                  foreach($arr as $img){                        
+                    $countSide++;
+                        $link_url = "";
+                        if($img['type_id'] == 2){
+                            $link_url = "su-kien/".$img['name_en']."-".$img['id'].".html";
+                        }
+                        if($img['type_id'] == 3){
+                            $link_url = $img['link_url'];
+                        }
+              ?>
+              <div class="item <?php echo $countSide == 1  ? "active" : ""; ?>">
+                <?php if($link_url!=""){ ?><a href="<?php echo $link_url;?>"><?php } ?>
+                  <img src="<?php echo $img['image_url']; ?>" alt="<?php echo $img['name_event']; ?>"/>
+               <?php if($link_url!=""){ ?></a><?php } ?>
+              </div>
+              <?php }} ?>              
+            </div>
+          
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+      </div>
+      <div class="banner-main2">
+        <?php $arrSmall = $model->getListBannerByPosition(2,-1); ?>
+        <?php
+          if(!empty($arrSmall)){           
+              foreach($arrSmall as $img1){                        
+                
+                    $link_url1 = "";
+                    if($img1['type_id'] == 2){
+                        $link_url1 = "su-kien/".$img1['name_en']."-".$img1['id'].".html";
+                    }
+                    if($img1['type_id'] == 3){
+                        $link_url1 = $img1['link_url'];
+                    }
+          ?>
+        <?php if($link_url1!=""){ ?><a href="<?php echo $link_url1;?>"><?php } ?>
+                  <img src="<?php echo $img1['image_url']; ?>" alt="<?php echo $img1['name_event']; ?>"/>
+                <?php if($link_url1!=""){ ?></a><?php } ?>
+        <?php }} ?>
+        
+      </div>
   </div>
 </div>
